da8de5abdd9842f26033c996fdf8d1bec85f33a8
Ivaylo <ivailorozenov@gmail.com>
2026-02-22 22:47:39 +0200

    Initial upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BirthdaySection from './components/BirthdaySection';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BirthdaySection />
+    </div>
+  );
+}
+
+export default App;
